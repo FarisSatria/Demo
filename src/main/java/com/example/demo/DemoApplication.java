bb29a166759b3bda44da850c2729186ebed6faef
FarisSatria <farissatria031@gmail.com>
2024-10-01 13:44:52 -0400

    Initial commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -14,7 +18,15 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public String helloWorld(){
-		return "Hello World";
+	public List<Student> studentList(){
+		return List.of(
+				new Student(
+						1L,
+						"Mariam",
+						"mariam.faris@gmail.com",
+						LocalDate.of(2007, Month.FEBRUARY, 5),
+						21
+				)
+		);
 	}
 }
